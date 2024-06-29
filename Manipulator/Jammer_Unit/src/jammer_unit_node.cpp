5d7b18ba4a6a2260e7933b42e171543c8bd79ed6
mungijeon <140574302+mungijeon@users.noreply.github.com>
2024-06-29 23:56:37 +0900

    24-06-29

--- Manipulator/Jammer_Unit/src/jammer_unit_node.cpp
+++ Manipulator/Jammer_Unit/src/jammer_unit_node.cpp
@@ -20,6 +20,8 @@ public:
     // 구독자 초기화: dynamixel_positions 토픽을 구독하고 콜백 함수 설정
     sub_theta1 = nh_.subscribe("position_1", 10, &DynamixelControl::commandCallback1, this);
     sub_theta2 = nh_.subscribe("position_2", 10, &DynamixelControl::commandCallback2, this);
+    //sub_theta1 = nh_.subscribe("topic_theta1", 10, &DynamixelControl::commandCallback1, this);
+    //sub_theta2 = nh_.subscribe("topic_theta2", 10, &DynamixelControl::commandCallback2, this);
   }
 
   // 콜백 함수: dynamixel_positions 토픽에서 메시지를 받을 때 호출
