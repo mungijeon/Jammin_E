16db49a72a4d2ab83ef173600a02b243c29738c2
mungijeon <140574302+mungijeon@users.noreply.github.com>
2024-08-25 15:27:58 +0900

    24-08-25

--- Manipulator/Jammer_Unit/src/jammer_unit_node.cpp
+++ Manipulator/Jammer_Unit/src/jammer_unit_node.cpp
@@ -1,30 +1,25 @@
-// ROS의 핵심 기능을 사용하기 위해 필요
 #include "ros/ros.h"
-// ROS 표준 메시지 타입인 'std_msgs' 패키지에서 'Int32MultiArray' 메시지 타입을 포함시키기 위한 헤더 파일을 포함
-// 'Int32MultiArrayLayout'는 여러 개의 32비트 정수('int32')를 배열 형태로 포함하는 메시지 타입이다.
-// 여러 개의 32비트 정수를 배열 형태로 전달하는 데 사용
 #include "std_msgs/Int32.h"
-// DynamixelCommand를 사용하기 위함
 #include "dynamixel_workbench_msgs/DynamixelCommand.h"
 
-// DynamixelControl 클래스 선언
 class DynamixelControl
 {
-// public 멤버는 구조체 또는 클래스 외부에서 접근할 수 있는 구조체 또는 클래스의 공개 멤버다.
 public:
-  // 생성자: 초기화 및 ROS 서비스 클라이언트와 구독자를 설정
   DynamixelControl()
   {
-    // 서비스 클라이언트 초기화: /dynamixel_workbench/dynamixel_command 서비스와 통신
     client = nh_.serviceClient<dynamixel_workbench_msgs::DynamixelCommand>("/dynamixel_workbench/dynamixel_command");
-    // 구독자 초기화: dynamixel_positions 토픽을 구독하고 콜백 함수 설정
-    sub_theta1 = nh_.subscribe("position_1", 10, &DynamixelControl::commandCallback1, this);
-    sub_theta2 = nh_.subscribe("position_2", 10, &DynamixelControl::commandCallback2, this);
-    //sub_theta1 = nh_.subscribe("topic_theta1", 10, &DynamixelControl::commandCallback1, this);
-    //sub_theta2 = nh_.subscribe("topic_theta2", 10, &DynamixelControl::commandCallback2, this);
+    sub_theta1 = nh_.subscribe("/position_1", 10, &DynamixelControl::commandCallback1, this);
+    sub_theta2 = nh_.subscribe("/position_2", 10, &DynamixelControl::commandCallback2, this);
+
+    /*********************************************************************
+      TOPIC LIST
+
+      sub theta1 : /position_1    from simulink
+      sub theta2 : /position_2    from simulink
+    *********************************************************************/
+
   }
 
-  // 콜백 함수: dynamixel_positions 토픽에서 메시지를 받을 때 호출
   void commandCallback1(const std_msgs::Int32::ConstPtr& msg)
   {
     setPosition(3, msg->data);
@@ -34,44 +29,28 @@ public:
     setPosition(4, msg->data);
   }
   
-  // 모터 위치 설정 함수
   void setPosition(int id, int position)
   {
-    // DynamixelCommand 서비스 요청 메시지 생성
     dynamixel_workbench_msgs::DynamixelCommand srv;
-    // 요청 명령을 빈 문자열로 설정
     srv.request.command = "";
-    // 요청 모터 ID 설정
     srv.request.id = id;
-    // 요청 주소 이름 설정: Goal_Position (목표 위치)
     srv.request.addr_name = "Goal_Position";
-    // 요청 값 설정: 위치
     srv.request.value = position;
 
-    // 서비스를 호출하여 요청을 보냄
     client.call(srv);
   }
 
-// private 멤버는 오직 클래스의 다른 멤버만 접근할 수 있는 비공개 멤버다.
-// 일반적으로 멤버 변수는 비공개로 하고, 멤버 함수는 공개하는 것이 일반적이다.
 private:
-  // ROS 노드 핸들러
   ros::NodeHandle nh_;
-  // 서비스 클라이언트
   ros::ServiceClient client;
-  // 구독자
   ros::Subscriber sub_theta1;
   ros::Subscriber sub_theta2;
 };
 
-// 메인 함수
 int main(int argc, char **argv)
 {
-  // ROS 노드 초기화
   ros::init(argc, argv, "jammer_unit_node");
-  // DynamixelControl 객체 생성
   DynamixelControl jammer_unit;
-  // ROS 이벤트 루프 시작: 콜백 함수가 호출될 수 있도록 함
   ros::spin();
   return 0;
 }
--- Manipulator/Radar_Unit/src/radar_unit_node.cpp
+++ Manipulator/Radar_Unit/src/radar_unit_node.cpp
@@ -1,37 +1,53 @@
 #include "ros/ros.h"
 #include "dynamixel_workbench_msgs/DynamixelCommand.h"
-// dynamixel_workbench_msgs::DynamixelStateList 메세지 타입을 사용하기 위함
-// 이는 다이나믹셀 상태 정보를 포함
 #include "dynamixel_workbench_msgs/DynamixelStateList.h"
-#include "std_msgs/Int32.h"       // 정수 R 값을 위한 메시지 타입
+#include "std_msgs/Int32.h"
 
 class DynamixelControl
 {
 public:
   DynamixelControl()
   {
-    // 서비스 클라이언트 생성: dynamixel_command 서비스를 호출하기 위한 클라이언트를 생성
     client = nh_.serviceClient<dynamixel_workbench_msgs::DynamixelCommand>("/dynamixel_workbench/dynamixel_command");
-    // 구독자 생성: dynamixel_state 토픽을 구독하고, 콜백 함수를 설정
     sub = nh_.subscribe("/dynamixel_workbench/dynamixel_state", 10, &DynamixelControl::Callback, this);
-    // topic1을 통해 구독
     sub_r = nh_.subscribe("/rf_layer", 10, &DynamixelControl::RCallback, this);    
-    // topic2를 통해 발행
     pub_theta1 = nh_.advertise<std_msgs::Int32>("/topic_theta1", 10);
     pub_theta2 = nh_.advertise<std_msgs::Int32>("/topic_theta2", 10);
     pub_r = nh_.advertise<std_msgs::Int32>("/topic_r", 10);
+
+    /*********************************************************************
+      TOPIC LIST
+
+      sub r      : /rf_layer        from matlab
+
+      pub theta1 : /topic_theta1    to simulink
+      pub theta2 : /topic_theta2    to simulink
+      pub r      : /topic_r         to simulink
+    *********************************************************************/
 
     ros::Duration(0.5).sleep();
     setSpeed(1,40);
     setSpeed(2,300);
     setPosition(1, 1000);
     setPosition(2, 0);
+
+    /*********************************************************************
+      INIT VALUE
+      
+      SPEED
+
+      Motor1(xy) : 40
+      Motor2(z)  : 300
+
+      POSITION
+
+      Motor1(xy) : 1000
+      Motor2(z)  : 0
+    *********************************************************************/
   }
 
-  // 콜백 함수 정의: dynamixel_state 메세지를 처리
   void Callback(const dynamixel_workbench_msgs::DynamixelStateList::ConstPtr& msg)
   {
-    // dynamixel_state 리스트 내의 각 상태 메세지를 반복 처리
     for (const auto& state : msg->dynamixel_state)
     {
       if (state.id == 1)
@@ -56,7 +72,6 @@ public:
     }
   }
 
-  // R 값을 받는 콜백 함수
   void RCallback(const std_msgs::Int32::ConstPtr& msg)
   {
     int R = msg->data;
@@ -64,17 +79,14 @@ public:
     ROS_INFO("theta1: %d", current_position1);
     ROS_INFO("theta2: %d", current_position2);
 
-    // R 값을 발행
     std_msgs::Int32 r_msg;
     r_msg.data = R;
     pub_r.publish(r_msg);
 
-    // 모터 1 위치를 발행
     std_msgs::Int32 theta1_msg;
     theta1_msg.data = current_position1;
     pub_theta1.publish(theta1_msg);
 
-    // 모터 2 위치를 발행
     std_msgs::Int32 theta2_msg;
     theta2_msg.data = current_position2;
     pub_theta2.publish(theta2_msg);
