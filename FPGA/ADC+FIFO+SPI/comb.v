e02facd38c1d1cfb88fcf59e7c248a2203c5bda3
leehyojeongggg <129163588+leehyojeongggg@users.noreply.github.com>
2024-08-28 06:07:56 +0900

    Create comb.v

---
+++ FPGA/ADC+FIFO+SPI/comb.v
@@ -0,0 +1,178 @@
+
+module SPI_Master
+  #(parameter SPI_MODE = 0,
+    parameter CLKS_PER_HALF_BIT = 1) //SPI Clock 주기의 반에 i_clk이 얼마나 들어가냐 
+  (
+   // Control/Data Signals,
+   input        i_Rst_L,     // FPGA Reset //reset 신호
+   input        i_Clk,       // clk_SPI
+   
+   // TX (MOSI) Signals
+   input [11:0]  i_TX_Byte,        // Byte to transmit on MOSI -MOSI핀으로 전송할 8비트 데이터
+   //input        i_TX_DV,          // Data Valid Pulse with i_TX_Byte -데이터 다 보냈다고 보내는 펄스인듯
+   output reg   o_TX_Ready,       // Transmit Ready for next byte -다음 바이트를 보낼 준비가 되었다(바이트를 비트로 바꾸기 위해서 필요?) 
+   
+   //MISO 안쓰니까 필요 없을듯
+   // RX (MISO) Signals
+   //output reg       o_RX_DV,     // Data Valid pulse (1 clock cycle)
+   //output reg [7:0] o_RX_Byte,   // Byte received on MISO
+   
+
+   // SPI Interface
+   output reg o_SPI_Clk, //SCK 만들어주기 위한거인듯
+   //input      i_SPI_MISO, 
+   output reg o_SPI_MOSI //이게 내가 필요한 핀일듯-slave로 전달하기 위한 핀
+   );
+
+  // SPI Interface (All Runs at SPI Clock Domain)
+  wire w_CPOL;     // Clock polarity
+  wire w_CPHA;     // Clock phase
+
+  reg [$clog2(CLKS_PER_HALF_BIT*2)-1:0] r_SPI_Clk_Count; 
+  reg r_SPI_Clk;
+  reg [4:0] r_SPI_Clk_Edges;
+  reg r_Leading_Edge;
+  reg r_Trailing_Edge;
+  reg       r_TX_DV;
+  reg [11:0] r_TX_Byte;
+
+  reg [2:0] r_RX_Bit_Count;
+  reg [3:0] r_TX_Bit_Count;
+
+   reg [11:0] lut[0:15]; //for LUT 
+  // CPOL: Clock Polarity
+  // CPOL=0 means clock idles at 0, leading edge is rising edge.
+  // CPOL=1 means clock idles at 1, leading edge is falling edge.
+  assign w_CPOL  = (SPI_MODE == 2) | (SPI_MODE == 3);
+
+  // CPHA: Clock Phase
+  // CPHA=0 means the "out" side changes the data on trailing edge of clock
+  //              the "in" side captures data on leading edge of clock
+  // CPHA=1 means the "out" side changes the data on leading edge of clock
+  //              the "in" side captures data on the trailing edge of clock
+  assign w_CPHA  = (SPI_MODE == 1) | (SPI_MODE == 3);
+
+
+
+
+  // Purpose: Generate SPI Clock correct number of times when DV pulse comes
+  //내가 원하는 SPI_clk(SCK) 만들어주는 부분
+  always @(posedge i_Clk or negedge i_Rst_L)
+  begin
+    if (~i_Rst_L) //active-low로 동작하는 reset
+    begin //초기
+      o_TX_Ready      <= 1'b0;
+      r_SPI_Clk_Edges <= 0;
+      r_Leading_Edge  <= 1'b0;
+      r_Trailing_Edge <= 1'b0;
+      r_SPI_Clk       <= w_CPOL; // assign default state to idle state, w_CPOL 0이면 clk값 0부터 시작, 1이면 clk값 1부터 시작
+      r_SPI_Clk_Count <= 0;
+    end
+    else
+    begin
+
+      // Default assignments
+      r_Leading_Edge  <= 1'b0;
+      r_Trailing_Edge <= 1'b0;
+      
+      if(r_SPI_Clk_Edges==0)
+      begin
+      r_SPI_Clk_Edges=24;
+      o_TX_Ready <= 1'b1;
+      end
+      else if (r_SPI_Clk_Edges > 0)
+      begin
+        o_TX_Ready <= 1'b0;
+        
+        if (r_SPI_Clk_Count == CLKS_PER_HALF_BIT*2-1) //r_SPI_Clk_count==7(시뮬레이션에서 4로해놔서)
+        begin
+          r_SPI_Clk_Edges <= r_SPI_Clk_Edges - 1'b1;
+          r_Trailing_Edge <= 1'b1; //trailing: 하강
+          r_SPI_Clk_Count <= 0;
+          r_SPI_Clk       <= ~r_SPI_Clk;
+        end
+        else if (r_SPI_Clk_Count == CLKS_PER_HALF_BIT-1) //3
+        begin
+          r_SPI_Clk_Edges <= r_SPI_Clk_Edges - 1'b1;
+          r_Leading_Edge  <= 1'b1; //leading: 상승
+          r_SPI_Clk_Count <= r_SPI_Clk_Count + 1'b1;
+          r_SPI_Clk       <= ~r_SPI_Clk;
+        end
+        else
+        begin
+          r_SPI_Clk_Count <= r_SPI_Clk_Count + 1'b1;
+        end
+      end
+      //  
+      
+      else if(r_SPI_Clk_Edges==24)
+      begin
+        o_TX_Ready      <= 1'b0;
+        r_SPI_Clk_Edges <= 24;  // Total # edges in one byte ALWAYS 16
+      end
+      
+  end // always @ (posedge i_Clk or negedge i_Rst_L)
+//여기까지 내가 원하는 SCK 생성 파트
+  end
+
+  // Purpose: Register i_TX_Byte when Data Valid is pulsed.
+  // Keeps local storage of byte in case higher level module changes the data
+  always@(posedge o_TX_Ready or negedge i_Rst_L)
+  begin
+    if (~i_Rst_L)
+    begin
+      r_TX_Byte <= 12'h00;
+      r_TX_DV   <= 1'b0;
+
+    end
+    else
+    begin
+        r_TX_Byte = i_TX_Byte;
+
+     end
+ end   
+    
+
+  // Purpose: Generate MOSI data
+  // Works with both CPHA=0 and CPHA=1
+  always@(posedge i_Clk or negedge i_Rst_L) //clk_SPI
+  begin
+    if (~i_Rst_L)
+    begin
+      o_SPI_MOSI     <= 1'b0;
+      r_TX_Bit_Count <= 4'b1011; // send MSb first(11) 
+    end
+    else
+    begin
+      // If ready is high, reset bit counts to default
+      if (o_TX_Ready)
+      begin
+        r_TX_Bit_Count <= 4'b1011; 
+      end
+      // Catch the case where we start transaction and CPHA = 0
+      else if ((r_Leading_Edge & w_CPHA) | (r_Trailing_Edge & ~w_CPHA)) //시뮬레이션에서 모드 3으로 해서 CPHA 계속 1임 
+      //CPHA 1일때는 상승에지일 때, CPHA 0일 때는 하강에지일 때 (CPOL 1일때로 가정하는건가?)
+      //1비트씩 수신
+      begin
+        r_TX_Bit_Count <= r_TX_Bit_Count - 1'b1;
+        o_SPI_MOSI     <= r_TX_Byte[r_TX_Bit_Count]; //나머지 7비트 전송 
+      end
+    end
+  end
+
+  
+  // Purpose: Add clock delay to signals for alignment.
+  always@(posedge i_Clk or negedge i_Rst_L)
+  begin
+    if (~i_Rst_L)
+    begin
+      o_SPI_Clk  <= w_CPOL;
+    end
+    else
+      begin
+        o_SPI_Clk <= r_SPI_Clk;
+      end // else: !if(~i_Rst_L)
+  end // always @ (posedge i_Clk or negedge i_Rst_L)
+  
+
+endmodule // SPI_Master
