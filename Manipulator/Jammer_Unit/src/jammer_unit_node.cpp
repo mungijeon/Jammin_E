bcd73af17a34a4cde21bf534bec594bd97370894
mungijeon <140574302+mungijeon@users.noreply.github.com>
2024-10-11 14:14:37 +0900

    24-10-11

--- Manipulator/Jammer_Unit/src/jammer_unit_node.cpp
+++ Manipulator/Jammer_Unit/src/jammer_unit_node.cpp
@@ -2,33 +2,77 @@
 #include "std_msgs/Int32.h"
 #include "dynamixel_workbench_msgs/DynamixelCommand.h"
 
+#include "serial/serial.h"
+
 class DynamixelControl
 {
 public:
   DynamixelControl()
   {
-    client = nh_.serviceClient<dynamixel_workbench_msgs::DynamixelCommand>("/dynamixel_workbench/dynamixel_command");
-    sub_theta1 = nh_.subscribe("/position_1", 10, &DynamixelControl::commandCallback1, this);
-    sub_theta2 = nh_.subscribe("/position_2", 10, &DynamixelControl::commandCallback2, this);
-
     /*********************************************************************
       TOPIC LIST
 
       sub theta1 : /position_1    from simulink
       sub theta2 : /position_2    from simulink
     *********************************************************************/
+    client     = nh_.serviceClient<dynamixel_workbench_msgs::DynamixelCommand>("/dynamixel_workbench/dynamixel_command");
+    
+    sub_theta1 = nh_.subscribe("/position_1", 10, &DynamixelControl::Callback1, this);
+    sub_theta2 = nh_.subscribe("/position_2", 10, &DynamixelControl::Callback2, this);
+    
+    /*********************************************************************
+      INIT VALUE
+      
+      SPEED
 
+      Motor3(xy) : 150
+      Motor4(z)  : 150
+
+      POSITION
+
+      Motor3(xy) : 1300
+      Motor4(z)  : 1600
+    *********************************************************************/
+    setSpeed(3,150);
+    setSpeed(4,150);
+    setPosition(3,1300); // 115
+    setPosition(4,1600); // 140
+    
+    /* Relay Module: Set Low voltage */
+    try
+    {
+      ser.setPort("/dev/ttyACM0");
+      ser.setBaudrate(9600);
+      serial::Timeout to = serial::Timeout::simpleTimeout(1000);
+      ser.setTimeout(to);
+      ser.open();
+      
+      if (ser.isOpen())
+      {
+        ROS_INFO("Serial Port opened successfully.");
+      }
+    }
+      catch (serial::IOException& e)
+      {
+        ROS_ERROR_STREAM("Unable to open port.");
+      }
+      
+    ser.write("L");
   }
 
-  void commandCallback1(const std_msgs::Int32::ConstPtr& msg)
+
+  /* Callback Function */
+  void Callback1(const std_msgs::Int32::ConstPtr& msg)
   {
     setPosition(3, msg->data);
   }
-  void commandCallback2(const std_msgs::Int32::ConstPtr& msg)
+  void Callback2(const std_msgs::Int32::ConstPtr& msg)
   {
     setPosition(4, msg->data);
   }
   
+  
+  /* Set Position & Speed */
   void setPosition(int id, int position)
   {
     dynamixel_workbench_msgs::DynamixelCommand srv;
@@ -39,12 +83,26 @@ public:
 
     client.call(srv);
   }
+  
+  void setSpeed(int id, int speed)
+  {
+    dynamixel_workbench_msgs::DynamixelCommand srv;
+    srv.request.command = "";
+    srv.request.id = id;
+    srv.request.addr_name = "Profile_Velocity";
+    srv.request.value = speed;
+
+    client.call(srv);
+  }
 
 private:
   ros::NodeHandle nh_;
   ros::ServiceClient client;
+  
   ros::Subscriber sub_theta1;
   ros::Subscriber sub_theta2;
+  
+  serial::Serial ser;
 };
 
 int main(int argc, char **argv)
